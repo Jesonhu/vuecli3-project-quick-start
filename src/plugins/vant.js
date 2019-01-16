3307a4b834f954bd09ec433479e357d1d15b8b61
Jeson <jesonhu_web@163.com>
2019-01-16 17:02:34 +0800

    chore: 版本更新

--- src/plugins/vant.js
+++ src/plugins/vant.js
@@ -1,4 +1,4 @@
 // @see [官网](https://youzan.github.io/vant/#/zh-CN/)
 // @see [github](https://github.com/youzan/vant/)
 // @see [方式一. 使用 babel-plugin-import (推荐)](https://youzan.github.io/vant/#/zh-CN/quickstart)
-import 'vant/lib/vant-css/index.css'
+// import 'vant/lib/vant-css/index.css'
